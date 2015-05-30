10f6d543413214e34dcf131318eb26e6808d55e4
František Dvořák <valtri@civ.zcu.cz>
2015-05-30 19:07:35 +0200

    Unqualified $::path is empty and path is required in exec.

--- manifests/historyserver/install.pp
+++ manifests/historyserver/install.pp
@@ -6,12 +6,14 @@ class spark::historyserver::install {
   include stdlib
   contain spark::common::postinstall
 
+  $path = '/sbin:/usr/sbin:/bin:/usr/bin'
+
   case $::osfamily {
     # Debian really fucked up design around postinstallation scripts
     'debian': {
       exec {'debian-fuckup':
         command => 'touch /etc/init.d/spark-history-server && chmod +x /etc/init.d/spark-history-server',
-        path    => $::path,
+        path    => $path,
         creates => '/etc/init.d/spark-history-server',
       }
       ->
@@ -21,7 +23,7 @@ class spark::historyserver::install {
       ->
       exec{'debian-restore-fuckup':
         command => 'mv -v /etc/init.d/spark-history-server.dpkg-dist /etc/init.d/spark-history-server',
-        path    => $::path,
+        path    => $path,
         onlyif  => 'test -f /etc/init.d/spark-history-server.dpkg-dist',
       }
     }
